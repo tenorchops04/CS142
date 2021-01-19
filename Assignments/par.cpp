b1b73bdd4829571cf071ead32a9c04fca4615943
Celvin Lizama Pena <cliza007@ti-05.cs.ucr.edu>
2021-01-19 14:03:25 -0800

    Finished parallel algorithm

--- Assignments/par.cpp
+++ Assignments/par.cpp
@@ -2,28 +2,44 @@
 #include <stdio.h>
 #include <math.h>
 #include <chrono>
+#include <ctime>
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
+#include "get_time.h"
 
-using namespace std::chrono;
+//using namespace std::chrono;
 using namespace std;
 
+long double reduce(int* A, int n){
+	long double segs = pow(10, 8);
+
+	if(n == 1)
+		return (sin(1+A[0]/segs) * log(1+A[0]/segs) + sin(1+(A[0]+1)/segs) * log(1+(A[0]+1)/segs)) / segs / 2;
+
+	long double L, R;
+	L = cilk_spawn reduce(A, n / 2);
+	R = reduce(A + n/2, n - n/2);
+	cilk_sync;
+	
+	return L + R;
+}
+
 int main(){
-	int segs = pow(10,8);
+	int n = 100000000;
 	long double sum = 0;
+	int* A = new int[n];
 
-	auto start = high_resolution_clock::now(); 
-
-	cilk_for(int i = 0; i < segs; i++){
-		sum += (sin(1 + i/segs) * log(1 + (i + 1)/segs)) / segs /2;
+	cilk_for(int i = 0; i < n; i++){
+		A[i] = i;
 	}
 
-	auto stop = high_resolution_clock::now(); 
+	timer t;
+	sum = reduce(A, n);	
+	t.stop();
 
-	auto duration = duration_cast<milliseconds>(stop - start);
-
-	cout << "Duration: " << duration.count() << endl;
+	cout << "Duration: " << t.get_total() << endl;
 	printf("%.10Lf\n", sum);
 
+	delete[] A;
 	return 0;
 }
