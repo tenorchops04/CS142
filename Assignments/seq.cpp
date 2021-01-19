aede059f8c2539312a71914196701455751490b3
Celvin Lizama Pena <cliza007@wch133-04.cs.ucr.edu>
2021-01-19 01:22:58 -0800

    Added folder for assignments and did #1 of 4.2

---
+++ Assignments/seq.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+int main(){
+	int segs = pow(10,8);
+	long double sum = 0;
+
+	auto start = high_resolution_clock::now(); 
+
+	for(int i = 0; i < segs; i++){
+		sum += (sin(1 + i/segs) * log(1 + (i + 1)/segs)) / segs /2;
+	}
+
+	auto stop = high_resolution_clock::now(); 
+
+	auto duration = duration_cast<milliseconds>(stop - start);
+
+	cout << "Duration: " << duration.count() << endl;
+	printf("%.10Lf\n", sum);
+
+	return 0;
+}
